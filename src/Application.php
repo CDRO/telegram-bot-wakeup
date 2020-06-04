7f9f5df192bad89390fffa2f89c2213b9a7f945d
Tizian Schmidlin <st@cabag.ch>
2020-06-04 10:27:38 +0200

    [TASK] Synchronize base application with 2FA

--- src/Application.php
+++ src/Application.php
@@ -18,7 +18,7 @@ class Application
         (new SecurityLayer)->check($securityLayerCallback);
 
         $this->client = $client;
-        $this->registry = new BasicRegistry($client, dirname(__DIR__) . '/static.json';
+        $this->registry = (new BasicRegistry($client, dirname(__DIR__) . '/static.json'))->load();
     }
 
     /**
@@ -27,14 +27,11 @@ class Application
      */
     public function runWebhook()
     {
-        $handler = new WebhookRegistry($this->registry);
-
-        $handler->handle();
+        (new WebhookRegistry($this->registry))->handle();
     }
 
     /**
-     *
-     *
+     * Run the 2FA distribution
      */
     public function run()
     {
